b3b3036a73408a99b5cd65d14e3ac124021db30c
yamileon <yamileon.xfz@gmail.com>
2019-06-27 10:01:26 +0100

    terafrom install script

---
+++ teraform/install_teraform_ubuntu.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+url=https://releases.hashicorp.com/terraform/0.12.3/terraform_0.12.3_linux_amd64.zip
+
+sudo apt update
+
+sudo apt upgrade -y
+
+sudo apt install -y unzip wget
+
+wget $url
+
+unzip terraform_*_linux_*.zip
+
+sudo mv terraform /usr/local/bin
+
+rm terraform_*_linux_*.zip
+
+terraform --version
