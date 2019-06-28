74ae020fd85434cb78bf34915580c5f7e5284f86
yamileon <yamileon.xfz@gmail.com>
2019-06-28 14:08:48 +0100

    pymsg

--- teraform/nsg.tf
+++ teraform/nsg.tf
@@ -58,7 +58,7 @@ resource "azurerm_network_security_group" "jenki_build" {
 }
 
 resource "azurerm_network_security_group" "pyserver" {
-  name                = "${var.prefix_py}-nsg"
+  name                = "${var.prefix_pyserver}-nsg"
   location            = "${azurerm_resource_group.main.location}"
   resource_group_name = "${azurerm_resource_group.main.name}"
 
